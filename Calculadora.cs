6ea56f1951c992cdf12ce10b66aa3dd7e904d923
CAUA ALVES DOS SANTOS <caua.asantos2@senacsp.edu.br>
2025-08-05 21:30:33 -0300

    Adiciona o calculo do botão Multiplicar

--- Calculadora.cs
+++ Calculadora.cs
@@ -38,7 +38,10 @@ namespace Calculadora
 
         private void btnMultiplicar_Click(object sender, EventArgs e)
         {
-          
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+            lblResultado.Text = (numero1 * numero2).ToString();
         }
     }
 }
